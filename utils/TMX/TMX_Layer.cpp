3aeee062352bb1ece648c820458b5e2160cdaf1c
bmarquis <bmarquismarkail@users.noreply.github.com>
2015-05-05 23:35:34 +0000

    General - Tabbed various spaces. General - Deleted miniz directory. TMX_Image.cpp - strtoul functions have base arguments other than 0.

--- utils/TMX/TMX_Layer.cpp
+++ utils/TMX/TMX_Layer.cpp
@@ -6,41 +6,41 @@ using namespace std;
 
 TMX_Layer::TMX_Layer()
 {
-    Opacity = 1;
-    Visible = true;
+	Opacity = 1;
+	Visible = true;
 }
 
 TMX_Layer::TMX_Layer(char *data, xml_node<> *node)
 {
-    Load_Layer(data, node);
+	Load_Layer(data, node);
 }
 
 void TMX_Layer::Load_Layer(char *data, xml_node<> *node)
 {
-    if(xml_attribute<> *iattr = node->first_attribute("name"))      Name      = iattr->value();
-    if(xml_attribute<> *iattr = node->first_attribute("opacity"))   Opacity   = atof(iattr->value()); else Opacity = 1.0;
-    if(xml_attribute<> *iattr = node->first_attribute("visible"))   Visible   = atoi(iattr->value()); else Visible = 1;
-    for(xml_node<> *iNode = node->first_node() ; iNode; iNode = iNode->next_sibling() )
-    {
-        if(!strcmp(iNode->name(), "data"))
-        {
-            TMX_Data tmp_Data(data, iNode);
-            Data.push_back(tmp_Data);
-        }
-    }
+	if(xml_attribute<> *iattr = node->first_attribute("name"))		Name		= iattr->value();
+	if(xml_attribute<> *iattr = node->first_attribute("opacity"))	Opacity		= atof(iattr->value()); else Opacity = 1.0;
+	if(xml_attribute<> *iattr = node->first_attribute("visible"))	Visible		= atoi(iattr->value()); else Visible = 1;
+	for(xml_node<> *iNode = node->first_node() ; iNode; iNode = iNode->next_sibling() )
+	{
+		if(!strcmp(iNode->name(), "data"))
+		{
+			TMX_Data tmp_Data(data, iNode);
+			Data.push_back(tmp_Data);
+		}
+	}
 }
 
 TMX_Data TMX_Layer::getData()
 {
-    return Data.at(0);
+	return Data.at(0);
 }
 
 bool TMX_Layer::isVisible()
 {
-    return Visible;
+	return Visible;
 }
 
 float TMX_Layer::getOpacity()
 {
-    return Opacity;
+	return Opacity;
 }
--- utils/TMX/TMX_Map.cpp
+++ utils/TMX/TMX_Map.cpp
@@ -11,128 +11,128 @@ using namespace std;
 
 TMX_Map::TMX_Map(const char* fn)
 {
-    Filename.assign(fn);
-    ifstream in_xml(Filename.c_str(), ifstream::in);
-    if (!in_xml.is_open())
-        exit(-1);
-    stringstream *ss_xml = new stringstream;
-    *ss_xml << in_xml.rdbuf();
-    char *xml_data = new char[ss_xml->str().length()+1];
-    strcpy(xml_data, ss_xml->str().c_str());
-    in_xml.close();
-    delete ss_xml;
-    Load_Map(xml_data);
+	Filename.assign(fn);
+	ifstream in_xml(Filename.c_str(), ifstream::in);
+	if (!in_xml.is_open())
+		exit(-1);
+	stringstream *ss_xml = new stringstream;
+	*ss_xml << in_xml.rdbuf();
+	char *xml_data = new char[ss_xml->str().length()+1];
+	strcpy(xml_data, ss_xml->str().c_str());
+	in_xml.close();
+	delete ss_xml;
+	Load_Map(xml_data);
 
 }
 
 TMX_Map::TMX_Map()
 {
-    Filename        = "";
-    Orientation     = "";
-    Width           = 0;
-    Height          = 0;
-    TileWidth       = 0;
-    TileHeight      = 0;
-    BackgroundColor = "";
+	Filename		= "";
+	Orientation		= "";
+	Width			= 0;
+	Height			= 0;
+	TileWidth		= 0;
+	TileHeight		= 0;
+	BackgroundColor = "";
 
 
 }
 
 void TMX_Map::Load_Map(char *data)
 {
-    rapidxml::xml_document<> *doc = new rapidxml::xml_document<>;
-    doc->parse<0>(data);
-    rapidxml::xml_node<> *pRoot = doc->first_node();
+	rapidxml::xml_document<> *doc = new rapidxml::xml_document<>;
+	doc->parse<0>(data);
+	rapidxml::xml_node<> *pRoot = doc->first_node();
 
-    if(pRoot->first_attribute("version"))         Version.assign         (pRoot->first_attribute("version")->value());
-    if(pRoot->first_attribute("orientation"))     Orientation.assign     (pRoot->first_attribute("orientation")->value());
-    if(pRoot->first_attribute("width"))           Width      = strtoul   (pRoot->first_attribute("width")->value(), NULL, 0);
-    if(pRoot->first_attribute("height"))          Height     = strtoul   (pRoot->first_attribute("height")->value(), NULL, 0);
-    if(pRoot->first_attribute("tilewidth"))       TileWidth  = strtoul   (pRoot->first_attribute("tilewidth")->value(), NULL, 0);
-    if(pRoot->first_attribute("tileheight"))      TileHeight = strtoul   (pRoot->first_attribute("tileheight")->value(), NULL, 0);
-    if(pRoot->first_attribute("backgroundcolor")) BackgroundColor.assign (pRoot->first_attribute("backgroundcolor")->value());
-    for (rapidxml::xml_node<> *iNode = pRoot->first_node() ; iNode ; iNode = iNode->next_sibling())
-    {
-        if (!strcmp(iNode->name(), "tileset"))
-        {
-            TMX_Tileset tmp_Tileset(data, iNode);
-            Tileset.push_back(tmp_Tileset);
-        }
-        if (!strcmp(iNode->name(), "layer"))
-        {
-            TMX_Layer tmp_Layer(data, iNode);
-            Layer.push_back(tmp_Layer);
-        }
+	if(pRoot->first_attribute("version"))			Version.assign(pRoot->first_attribute("version")->value());
+	if(pRoot->first_attribute("orientation"))		Orientation.assign(pRoot->first_attribute("orientation")->value());
+	if(pRoot->first_attribute("width"))				Width = strtoul(pRoot->first_attribute("width")->value(), NULL, 0);
+	if(pRoot->first_attribute("height"))			Height = strtoul(pRoot->first_attribute("height")->value(), NULL, 0);
+	if(pRoot->first_attribute("tilewidth"))			TileWidth = strtoul(pRoot->first_attribute("tilewidth")->value(), NULL, 0);
+	if(pRoot->first_attribute("tileheight"))		TileHeight = strtoul(pRoot->first_attribute("tileheight")->value(), NULL, 0);
+	if(pRoot->first_attribute("backgroundcolor"))	BackgroundColor.assign (pRoot->first_attribute("backgroundcolor")->value());
+	for (rapidxml::xml_node<> *iNode = pRoot->first_node() ; iNode ; iNode = iNode->next_sibling())
+	{
+		if (!strcmp(iNode->name(), "tileset"))
+		{
+			TMX_Tileset tmp_Tileset(data, iNode);
+			Tileset.push_back(tmp_Tileset);
+		}
+		if (!strcmp(iNode->name(), "layer"))
+		{
+			TMX_Layer tmp_Layer(data, iNode);
+			Layer.push_back(tmp_Layer);
+		}
 
-    }
+	}
 }
 
 string TMX_Map::getMapFile()
 {
-    return Filename;
+	return Filename;
 }
 
 string TMX_Map::getVersion()
 {
-    return Version;
+	return Version;
 }
 
 string TMX_Map::getOrientation()
 {
-    return Orientation;
+	return Orientation;
 }
 
 unsigned long TMX_Map::getWidth()
 {
-    return Width;
+	return Width;
 }
 
 unsigned long TMX_Map::getHeight()
 {
-    return Height;
+	return Height;
 }
 
 unsigned long TMX_Map::getTileWidth()
 {
-    return TileWidth;
+	return TileWidth;
 }
 
 unsigned long TMX_Map::getTileHeight()
 {
-    return TileHeight;
+	return TileHeight;
 }
 
 string TMX_Map::getBackgroundColor()
 {
-    return BackgroundColor;
+	return BackgroundColor;
 }
 
 TMX_Tileset TMX_Map::getTileset(unsigned index)
 {
-    return Tileset.at(index);
+	return Tileset.at(index);
 }
 
 TMX_Layer TMX_Map::getLayer(unsigned index)
 {
-    return Layer.at(index);
+	return Layer.at(index);
 }
 
 unsigned TMX_Map::findTileset(unsigned int GID)
 {
-    for( int i = Tileset.size()-1; i > -1; --i)
-    {
-        if (GID >= Tileset[i].getGID())
-            return i;
-    }
-    return -1;
+	for( int i = Tileset.size()-1; i > -1; --i)
+	{
+		if (GID >= Tileset[i].getGID())
+			return i;
+	}
+	return -1;
 }
 
 unsigned TMX_Map::getNumLayers()
 {
-    return Layer.size();
+	return Layer.size();
 }
 
 unsigned TMX_Map::getNumTilesets()
 {
-    return Tileset.size();
+	return Tileset.size();
 }
