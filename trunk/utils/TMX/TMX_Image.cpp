8f44e417bd46f1fd47dfc344808aeca4d70d83a1
bmarquis <bmarquis@97917899-218b-4479-a890-b926c5742252>
2015-05-05 23:35:34 +0000

    General - Tabbed various spaces. General - Deleted miniz directory. TMX_Image.cpp - strtoul functions have base arguments other than 0.

--- trunk/utils/TMX/TMX_Image.cpp
+++ trunk/utils/TMX/TMX_Image.cpp
@@ -10,42 +10,42 @@ TMX_Image::TMX_Image()
 
 TMX_Image::TMX_Image(char *data, xml_node<> *node)
 {
-    Load_Image(data, node);
+	Load_Image(data, node);
 }
 
 void TMX_Image::Load_Image(char *data, xml_node<> *node)
 {
-    if(xml_attribute<> *iattr = node->first_attribute("format"))   Format   = iattr->value();
-    if(xml_attribute<> *iattr = node->first_attribute("id"))       ID       = iattr->value();
-    if(xml_attribute<> *iattr = node->first_attribute("source"))   Source   = iattr->value();
-    if(xml_attribute<> *iattr = node->first_attribute("trans"))    Trans    = strtoul( iattr->value(), NULL, 16);
-    if(xml_attribute<> *iattr = node->first_attribute("width"))    Width    = strtoul( iattr->value(), NULL, 0 );
-    if(xml_attribute<> *iattr = node->first_attribute("height"))   Height   = strtoul( iattr->value(), NULL, 0 );
-    for(xml_node<> *iNode = node->first_node() ; iNode; iNode = iNode->next_sibling() )
-    {
-        if(!strcmp(iNode->name(), "image"))
-        {
-            TMX_Data tmp_Data(data, node);
-            Data.push_back(tmp_Data);
-        }
-    }
+	if(xml_attribute<> *iattr = node->first_attribute("format"))	Format	= iattr->value();
+	if(xml_attribute<> *iattr = node->first_attribute("id"))		ID		= iattr->value();
+	if(xml_attribute<> *iattr = node->first_attribute("source"))	Source	= iattr->value();
+	if(xml_attribute<> *iattr = node->first_attribute("trans"))		Trans	= strtoul( iattr->value(), NULL, 16);
+	if(xml_attribute<> *iattr = node->first_attribute("width"))		Width	= strtoul( iattr->value(), NULL, 10 );
+	if(xml_attribute<> *iattr = node->first_attribute("height"))	Height	= strtoul( iattr->value(), NULL, 10 );
+	for(xml_node<> *iNode = node->first_node() ; iNode; iNode = iNode->next_sibling() )
+	{
+		if(!strcmp(iNode->name(), "image"))
+		{
+			TMX_Data tmp_Data(data, node);
+			Data.push_back(tmp_Data);
+		}
+	}
 }
 
 
 string TMX_Image::getFilename()
 {
-    return Source;
+	return Source;
 }
 unsigned long TMX_Image::getWidth()
 {
-    return Width;
+	return Width;
 }
 unsigned long TMX_Image::getHeight()
 {
-    return Height;
+	return Height;
 }
 
 unsigned long TMX_Image::getTrans()
 {
-    return Trans;
+	return Trans;
 }
