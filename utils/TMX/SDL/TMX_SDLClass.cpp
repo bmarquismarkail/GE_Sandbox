e47b91a96f4fb26824251133fa2398aca44a4c7e
bmarquis <bmarquismarkail@users.noreply.github.com>
2015-06-06 22:49:06 +0000

    General - Tabbed various spaces. General - Deleted miniz directory. TMX_Image.cpp - strtoul functions have base arguments other than 0.

--- utils/TMX/SDL/TMX_SDLClass.cpp
+++ utils/TMX/SDL/TMX_SDLClass.cpp
@@ -9,15 +9,15 @@
 
 
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-rmask = 0xff000000;
-gmask = 0x00ff0000;
-bmask = 0x0000ff00;
-amask = 0x000000ff;
+	rmask = 0xff000000;
+	gmask = 0x00ff0000;
+	bmask = 0x0000ff00;
+	amask = 0x000000ff;
 #else
-#define    rmask 0x000000ff
-#define    gmask 0x0000ff00
-#define    bmask 0x00ff0000
-#define    amask 0xff000000
+	rmask = 0x000000ff;
+	gmask = 0x0000ff00;
+	bmask = 0x00ff0000;
+	amask = 0xff000000;
 #endif
 
 SDL_TMXMap::SDL_TMXMap()
