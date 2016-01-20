95efeabd0d419eb53e372f3c3baf228ae51fb65d
bmarquis <bmarquis@97917899-218b-4479-a890-b926c5742252>
2016-01-20 19:02:34 +0000

    adding Linux Branch

---
+++ branches/Linux_64-MarquisFourteen/utils/TMX/TMX_Map.h
@@ -0,0 +1,242 @@
+#ifndef TMX_MAP_H_INCLUDED
+#define TMX_MAP_H_INCLUDED
+
+#include <string>
+#include <vector>
+using namespace std;
+
+#include <rapidxml/rapidxml.hpp>
+using namespace rapidxml;
+
+struct TMX_Property
+{
+	string name;
+	string value;
+};
+
+struct TMX_Properties
+{
+	vector<TMX_Property> Property;
+};
+
+struct TMX_TileOffset
+{
+	long X;
+	long Y;
+};
+
+class TMX_Tile
+{
+public:
+	TMX_Tile();
+	TMX_Tile(char *data, xml_node<> *node);
+	void Load_Tile(char *data, xml_node<> *node);
+private:
+	unsigned int			ID;
+	vector<unsigned int[4]> Terrain;
+	string					Probability;
+};
+
+class TMX_Data
+{
+public:
+	TMX_Data();
+	TMX_Data(char *data, xml_node<> *node);
+	void Load_Data(char *data, xml_node<> *node);
+	string getEncoding();
+	string getCompression();
+	string getData();
+	vector<TMX_Tile> getTileData();
+private:
+	string Encoding;
+	string Compression;
+	string Value;
+	vector<TMX_Tile> Tile;
+};
+
+class TMX_Image
+{
+public:
+	TMX_Image();
+	TMX_Image(char* data, xml_node<> *node);
+	void Load_Image(char* data, xml_node<> *node);
+	string getFilename();
+	unsigned long getWidth();
+	unsigned long getHeight();
+	unsigned long getTrans();
+private:
+	string Format;
+	string ID;
+	string Source;
+	unsigned long Trans;
+	unsigned long Width;
+	unsigned long Height;
+	vector<TMX_Data> Data;
+};
+
+struct TMX_Point
+{
+	long X;
+	long Y;
+};
+
+struct TMX_Polyline
+{
+	vector<TMX_Point> Point;
+};
+
+struct TMX_Polygon
+{
+	vector<TMX_Point> Point;
+};
+
+struct TMX_Ellipse
+{
+	long X;
+	long Y;
+	unsigned long Width;
+	unsigned long Height;
+};
+
+struct TMX_Object
+{
+
+	string name;
+	string type;
+	long X;
+	long Y;
+	unsigned long Width;
+	unsigned long Height;
+	int Rotation;
+	unsigned int gid;
+	bool Visible;
+	vector<TMX_Properties> Properties;
+	vector<TMX_Ellipse>	   Ellipse;
+	vector<TMX_Polygon>	   Polygon;
+	vector<TMX_Polyline>   Polyline;
+	vector<TMX_Image>	   Image;
+};
+
+struct TMX_ObjectGroup
+{
+	string name;
+	string color;
+	unsigned int X;
+	unsigned int Y;
+	long Width;
+	long Height;
+	float Opacity;
+	bool Visible;
+};
+
+struct TMX_Imagelayer
+{
+   string Name;
+   float  Opacity;
+   bool	  Visible;
+   vector<TMX_Properties> Property;
+   vector<TMX_Image>	  Image;
+};
+
+struct TMX_Terrain
+{
+	string name;
+	unsigned int		   ID;
+	vector<TMX_Properties> Properties;
+};
+
+struct TMX_TerrainType
+{
+	vector<TMX_Terrain> Terrain;
+};
+
+class TMX_Tileset
+{
+public:
+	TMX_Tileset();
+	TMX_Tileset(char *data, xml_node<> *node);
+	void Load_Tileset(char *data, xml_node<> *node);
+	string getName();
+	TMX_Image getImage(unsigned index);
+	unsigned long getTileWidth();
+	unsigned long getTileHeight();
+	unsigned int getSpacing();
+	unsigned int getGID();
+	string getSource();
+private:
+	unsigned int firstgid;
+	string source;
+	string name;
+	unsigned long tilewidth;
+	unsigned long tileheight;
+	unsigned int spacing;
+	unsigned int margin;
+	vector<TMX_TileOffset>	Offset;
+	vector<TMX_Properties>	Properties;
+	vector<TMX_Image>		Image;
+	vector<TMX_TerrainType> Terraintype;
+	vector<TMX_Tile>		Tile;
+};
+
+
+class TMX_Layer
+{
+public:
+	TMX_Layer();
+	TMX_Layer (char *data, xml_node<> *node);
+	void Load_Layer(char *data, xml_node<> *node);
+	unsigned long getWidth();
+	unsigned long getHeight();
+	TMX_Data getData();
+	bool isVisible();
+	float getOpacity();
+private:
+	string Name;
+	unsigned int		   X;
+	unsigned int		   Y;
+	unsigned long		   Width;
+	unsigned long		   Height;
+	float				   Opacity;
+	bool				   Visible;
+	vector<TMX_Properties> Properties;
+	vector<TMX_Data>	   Data;
+};
+
+class TMX_Map
+{
+public:
+	TMX_Map();
+	TMX_Map(const char* fn);
+	void Load_Map(char* data);
+	string getMapFile();
+	string getVersion();
+	string getOrientation();
+	unsigned long getWidth();
+	unsigned long getHeight();
+	unsigned long getTileWidth();
+	unsigned long getTileHeight();
+	string getBackgroundColor();
+	TMX_Tileset getTileset(unsigned index);
+	TMX_Layer getLayer(unsigned index);
+	unsigned findTileset(unsigned int GID);
+	unsigned getNumLayers();
+	unsigned getNumTilesets();
+private:
+	string Filename;
+	string Version;
+	string Orientation;
+	unsigned long Width;
+	unsigned long Height;
+	unsigned long TileWidth;
+	unsigned long TileHeight;
+	string BackgroundColor;
+	vector<TMX_Properties>	Properties;
+	vector<TMX_Tileset>		Tileset;
+	vector<TMX_Layer>		Layer;
+	vector<TMX_ObjectGroup> ObjGroup;
+	vector<TMX_Imagelayer>	ImgLayer;
+};
+
+
+
+#endif // TMX_MAP_H_INCLUDED
---
+++ branches/Linux_64-MarquisFourteen/utils/TMX/TMX_Utils.cpp
@@ -0,0 +1,69 @@
+#include "TMX_Utils.h"
+
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+#include <miniz/miniz.h>
+//#include <miniz/utils/gzip.c>
+
+void TMX_Decode(unsigned char* input, unsigned char* output, unsigned len)
+{
+    base64::decoder decode;
+    base64_init_decodestate(&decode._state);
+    decode.decode( (char*)input , len , (char*)output );
+}
+
+void TMX_Uncompress(unsigned char* input, unsigned char* output, unsigned int in_size, unsigned int out_size, const char* Scheme)
+{
+    if(!strcmp(Scheme, ""))
+    {
+        memcpy(output, input, in_size);
+        return;
+    }
+    if(!strcmp(Scheme, "zlib"))
+    {
+        unsigned long pLen = out_size;
+        int iserror = uncompress(output, &pLen, input, in_size);
+        if(iserror)
+            cout << "miniz error:" << iserror;
+        return;
+    }
+    if(!strcmp(Scheme, "gzip"))
+    {
+        z_stream strm  = {0};
+        strm.total_in  = strm.avail_in  = in_size - 10; //I am pretty sure this was the last problem I had. Need to document why it was one.
+        strm.total_out = strm.avail_out = out_size;
+        strm.next_in   = input + 10;
+        strm.next_out  = output;
+
+        strm.zalloc = Z_NULL;
+        strm.zfree  = Z_NULL;
+        strm.opaque = Z_NULL;
+
+        int err = -1;
+        int ret = -1;
+
+        err = inflateInit2(&strm,-15);
+        if (err == Z_OK)
+        {
+            err = inflate(&strm, Z_FINISH);
+            if (err == Z_STREAM_END)
+            {
+                ret = strm.total_out;
+            }
+            else
+            {
+                inflateEnd(&strm);
+            }
+        }
+        else
+        {
+            inflateEnd(&strm);
+        }
+
+        inflateEnd(&strm);
+        return;
+    }
+}
+
